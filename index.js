1f8c4bc5b6858977f4a78bfae833ce57e13f420e
robertsonc2-stack <robertsonc2@students.hcboe.net>
2025-09-30 09:38:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      datetime.textContent = `📅 It Will be Announced in: ${days}d ${hours}h ${minutes}m ${seconds}s`;
+      datetime.textContent = `📅 It will be Announced in: ${days}d ${hours}h ${minutes}m ${seconds}s`;
     }
   }, 1000);
 
