7876b382632c2a0c4a02c9cb63aa303179ba0c6b
robertsonc2-stack <robertsonc2@students.hcboe.net>
2025-09-30 08:30:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const datetime = document.getElementById("datetime");
   const xLink = document.getElementById("x-link");
 
-  message.textContent = "🚧 To be announced soon 🚧";
+  message.textContent = "🚧 To be announced soon. 🚧";
 
   // Set your event date & time here
   const target = new Date("October 31, 2025 18:00:00").getTime();
